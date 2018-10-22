4e55f580e5175c0fca852988bfa3b682923525d9
castrojonathanjames <castro.jonathanjames@gmail.com>
2018-10-22 14:28:39 +0800

    student info

--- resources/views/studentinfo.blade.php
+++ resources/views/studentinfo.blade.php
@@ -1,40 +1,57 @@
-<center>
-<table>
-	<th>
-		<td><strong>MY INFORMATION</strong></td>
-	</th>
-	<tr>
-		<td><strong>College:</strong></td>
-		<td><strong><i>{{$student->college}}</i></strong></td>
-	</tr>
-	<tr>
-		<td><strong>Course:</strong></td>
-		<td><strong><i>{{$student->course}}</i></strong></td>
-	</tr>
-	<tr>
-		<td><strong>Student Number:</strong></td>
-		<td><strong><i>{{$student->studentnumber}}</i></strong></td>
-	</tr>
-	<tr>
-		<td><strong>Name:</strong></td>
-		<td><strong><i>{{$student->name}}</i></strong></td>
-	</tr>
-	<tr>
-		<td><strong>Address:</strong></td>
-		<td><strong><i>{{$student->address}}</i></strong></td>
-	</tr>
-	<tr>
-		<td><strong>Phone Number:</strong></td>
-		<td><strong><i>{{$student->phonenumber}}</i></strong></td>
-	</tr>
-	<tr>
-		<td><strong>Email Address:</strong></td>
-		<td><strong><i>{{$student->email}}</i></strong></td>
-	</tr>
-	<tr>
-		<td><strong>Gender:</strong></td>
-		<td><strong><i>{{$student->gender}}</i></strong></td>
-	</tr>
+@extends('layout')
 
-</table>
-</center>
+@section('css')
+
+@endsection
+
+@section('javascript')
+
+@endsection
+
+@section('content')
+    <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" >
+        <thead>
+        <tr>
+            <th colspan="2" class="text-center">
+                <b> MY INFORMATION </b>
+            </th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td><strong>College:</strong></td>
+            <td><strong>{{$student->college}}</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Course:</strong></td>
+            <td><strong>{{$student->course}}</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Student Number:</strong></td>
+            <td><strong>{{$student->studentnumber}}</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Name:</strong></td>
+            <td><strong>{{$student->name}}</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Address:</strong></td>
+            <td><strong>{{$student->address}}</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Phone Number:</strong></td>
+            <td><strong>{{$student->phonenumber}}</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Email Address:</strong></td>
+            <td><strong>{{$student->email}}</strong></td>
+        </tr>
+        <tr>
+            <td><strong>Gender:</strong></td>
+            <td><strong>{{$student->gender}}</strong></td>
+        </tr>
+        </tbody>
+
+    </table>
+@endsection
