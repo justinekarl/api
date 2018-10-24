2e18b02e1cfdd8cb1ac62e278349ade32b36ce9f
castrojonathanjames <castro.jonathanjames@gmail.com>
2018-10-24 09:56:17 +0800

    resize display resume

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -28,84 +28,53 @@
 @endsection
 
 @section('content')
-    <!-- Main content -->
-    <section class="content">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <div class="container-fluid">
-                    <div class="panel panel-flat">
-                        <div class="panel-body">
-                            {{--<div class="panel-heading">
-                                <div class="row">
-                                    <div class="col-lg-10 col-md-10 col-sm-10">
-                                        <section class="content-header">
-                                            <h1>
-                                                <i class="fa fa-file-pdf-o"></i> Documents
-                                            </h1>
-                                        </section>
-                                    </div>
-                                </div>
+    <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" >
+        <thead>
+        <tr>
+            <th class="text-center" > Student </th>
+            <th class="text-center">Actions</th>
+        </tr>
+        </thead>
 
-                            </div>--}}
+        <tbody>
+        @foreach($students as $student)
 
-                            <div class="content">
-                                <div class="panel-body">
-                                    <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" >
-                                        <thead>
-                                        <tr>
-                                            <th class="text-center" > Student </th>
-                                            <th class="text-center">Actions</th>
-                                        </tr>
-                                        </thead>
-
-                                        <tbody>
-                                        @foreach($students as $student)
-
-                                                <tr>
-                                                    <td>
-                                                        <div class="row text-center">
-                                                            Student Name : {{$student->student_name}}
-                                                        </div>
-
-                                                        <div class="row text-center">
-                                                            College : {{$student->college}}
-                                                        </div>
-
-                                                        <div class="row text-center">
-                                                            Course : {{$student->course}}
-                                                        </div>
-                                                    </td>
-                                                    <td class="text-center">
-                                                        <div class="btn-group">
-                                                            <a href="#" class="btn btn-primary btn-sm company-user-approve" data-student_id = "{{$student->student_id}}" data-company_id = "{{$company_id}}">
-                                                                <i class="glyphicon glyphicon-ok"></i>
-                                                            </a>
-
-                                                            <a href="#" class="btn btn-danger btn-sm company-user-decline" data-student_id = "{{$student->student_id}}" data-company_id = "{{$company_id}}">
-                                                                <i class="glyphicon glyphicon-remove"></i>
-                                                            </a>
-                                                            {{--<a href="{{(isset($student) && $student != null && "" != $student->path) ? $fileManager->getFileUrl("files/documents", $student->path,$student->student_id) :
-                                                            $templatePlugin->rootLocation()."/css/images/default_image.png"}}" class="btn btn-success btn-sm documents-download" download="{{$student->path}}"><i class="fa fa-download"></i></a>--}}
-
-                                                            {{--<a href="#" class="btn btn-success btn-sm documents-download" data-download_location="{{(isset($student) && $student != null && "" != $student->path) ? $fileManager->getFileUrl("files/documents", $student->path,$student->student_id) :
-                                                            $templatePlugin->rootLocation()."/css/images/default_image.png"}}"><i class="fa fa-download"></i></a>--}}
-                                                        </div><br>
-                                                        <iframe class="doc" src="{{'https://docs.google.com/gview?url=http://18.191.44.167/storage/files/documents/'.$student->student_id.'/'.$student->path.'&embedded=true'}}"></iframe>
-                                                    </td>
-                                                </tr>
-
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
+            <tr>
+                <td>
+                    <div class="row text-center">
+                        Student Name : {{$student->student_name}}
                     </div>
-                </div>
-            </div>
-        </div>
-    </section>
+
+                    <div class="row text-center">
+                        College : {{$student->college}}
+                    </div>
+
+                    <div class="row text-center">
+                        Course : {{$student->course}}
+                    </div>
+                </td>
+                <td class="text-center">
+                    <div class="btn-group">
+                        <a href="#" class="btn btn-primary btn-sm company-user-approve" data-student_id = "{{$student->student_id}}" data-company_id = "{{$company_id}}">
+                            <i class="glyphicon glyphicon-ok"></i>
+                        </a>
+
+                        <a href="#" class="btn btn-danger btn-sm company-user-decline" data-student_id = "{{$student->student_id}}" data-company_id = "{{$company_id}}">
+                            <i class="glyphicon glyphicon-remove"></i>
+                        </a>
+                        {{--<a href="{{(isset($student) && $student != null && "" != $student->path) ? $fileManager->getFileUrl("files/documents", $student->path,$student->student_id) :
+                        $templatePlugin->rootLocation()."/css/images/default_image.png"}}" class="btn btn-success btn-sm documents-download" download="{{$student->path}}"><i class="fa fa-download"></i></a>--}}
+
+                        {{--<a href="#" class="btn btn-success btn-sm documents-download" data-download_location="{{(isset($student) && $student != null && "" != $student->path) ? $fileManager->getFileUrl("files/documents", $student->path,$student->student_id) :
+                        $templatePlugin->rootLocation()."/css/images/default_image.png"}}"><i class="fa fa-download"></i></a>--}}
+                    </div><br>
+                    <iframe class="doc" src="{{'https://docs.google.com/gview?url=http://18.191.44.167/storage/files/documents/'.$student->student_id.'/'.$student->path.'&embedded=true'}}"></iframe>
+                </td>
+            </tr>
+
+        @endforeach
+        </tbody>
+    </table>
 @endsection
 
 
